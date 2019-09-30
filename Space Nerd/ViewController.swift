10bb37c31c0a8fdf9c6e2fa12e2eba3082732e78
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-09-30 21:51:10 +0300

    Initial Commit

---
+++ Space Nerd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Space Nerd
+//
+//  Created by Petrina on 30.09.19.
+//  Copyright © 2019 Ralitsa Petrina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
