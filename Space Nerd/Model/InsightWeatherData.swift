d2d5cdab7aeaeb33bb61c1ed40b2471ec0fdb8e1
Ralitsa Petrina <ralitsa.petrina@gmail.com>
2019-10-01 22:48:28 +0300

    Day 2
    - Worked on structuring the data model
    
    To do:
    
    - Need to research more ways to parse JSON data or figure out the logistics of taking the sol_keys values and using them for the names in the decodable struct

--- Space Nerd/Model/InsightWeatherData.swift
+++ Space Nerd/Model/InsightWeatherData.swift
@@ -8,6 +8,34 @@
 
 import Foundation
 
+
+struct insightData: Decodable {
+    var sol0: solData
+    var sol1: solData
+    var sol2: solData
+    var sol3: solData
+    var sol4: solData
+    var sol5: solData
+    var sol6: solData
+    var sol_keys: Array<Int>
+    var validity_checks: validityChecks
+
+}
+
+struct validityChecks: Decodable {
+    
+}
+
 struct solData: Decodable {
-    var sol1: String
+    var AT: tempData  // temperature data
+    var HWS: tempData // horizontal wind speed data
+    var PRE: tempData // atmospheric pressur data
+    var Season: String
+}
+
+struct tempData: Decodable {
+    var av: Float
+    var ct: Int
+    var mn: Float
+    var mx: Float
 }
